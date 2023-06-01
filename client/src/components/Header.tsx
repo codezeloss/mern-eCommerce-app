b43c949030d709f873538175bbf29151242a697b
codezeloss <elhoussaineaitX@gmail.com>
2023-06-01 16:07:19 +0000

    Starting with the FE

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <>
+      <header className="">
+        <div className="flex items-center justify-between text-xs">
+          <p>Free Shipping Over $100 & Free Returns</p>
+
+          <div className="flex items-center space-x-4">
+            <p>Hotline: (212) 6 6666 6666</p>
+            <p>English</p>
+            <p>USD $</p>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
