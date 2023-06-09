1886512a88a466c60ce442243bfc304c7bec1d73
codezeloss <elhoussaineaitX@gmail.com>
2023-06-09 12:45:04 +0000

    debugging & refactoring some files

--- client/src/components/Product/productData.ts
+++ client/src/components/Product/productData.ts
@@ -1,30 +1,30 @@
 export const productData = [
   {
-    image: "../../public/images/watch.jpg",
+    image: "../../../public/images/watch.jpg",
     brand: "Havells",
     title: "Honor T1 7.0.1 GB RAM 8 GB ROM...",
     price: "100.00",
   },
   {
-    image: "../../public/images/watch.jpg",
+    image: "../../../public/images/watch.jpg",
     brand: "Havells",
     title: "Honor T1 7.0.1 GB RAM 8 GB ROM...",
     price: "100.00",
   },
   {
-    image: "../../public/images/watch.jpg",
+    image: "../../../public/images/watch.jpg",
     brand: "Havells",
     title: "Honor T1 7.0.1 GB RAM 8 GB ROM...",
     price: "100.00",
   },
   {
-    image: "../../public/images/watch.jpg",
+    image: "../../../public/images/watch.jpg",
     brand: "Havells",
     title: "Honor T1 7.0.1 GB RAM 8 GB ROM...",
     price: "100.00",
   },
   {
-    image: "../../public/images/watch.jpg",
+    image: "../../../public/images/watch.jpg",
     brand: "Havells",
     title: "Honor T1 7.0.1 GB RAM 8 GB ROM...",
     price: "100.00",
